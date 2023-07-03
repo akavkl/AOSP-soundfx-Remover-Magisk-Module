837ef1ecbdb7f29a3fc0bfee5cd56a12671c9cab
Swarnim Sushobhit Lakra <swarnim68@gmail.com>
2023-07-03 19:47:27 +0530

    Update aml.sh

--- aml.sh
+++ aml.sh
@@ -1,47 +1,88 @@
-MODPATH=${0%/*}
+[ -z $MODPATH ] && MODPATH=${0%/*}
 
 # destination
-MODAEC=`find $MODPATH/system -type f -name *audio*effects*.conf`
-MODAEX=`find $MODPATH/system -type f -name *audio*effects*.xml`
+MODAEC=`find $MODPATH -type f -name *audio*effects*.conf`
+MODAEX=`find $MODPATH -type f -name *audio*effects*.xml`
 
 # function
 remove_conf() {
-for RMVS in $RMV; do
-  sed -i "s/$RMVS/removed/g" $MODAEC
+for RMV in $RMVS; do
+  sed -i "s|$RMV|removed|g" $MODAEC
 done
-sed -i 's/path \/vendor\/lib\/soundfx\/removed//g' $MODAEC
-sed -i 's/path \/system\/lib\/soundfx\/removed//g' $MODAEC
-sed -i 's/path \/vendor\/lib\/removed//g' $MODAEC
-sed -i 's/path \/system\/lib\/removed//g' $MODAEC
-sed -i 's/library removed//g' $MODAEC
-sed -i 's/uuid removed//g' $MODAEC
+sed -i 's|path /vendor/lib/soundfx/removed||g' $MODAEC
+sed -i 's|path /system/lib/soundfx/removed||g' $MODAEC
+sed -i 's|path /vendor/lib/removed||g' $MODAEC
+sed -i 's|path /system/lib/removed||g' $MODAEC
+sed -i 's|library removed||g' $MODAEC
+sed -i 's|uuid removed||g' $MODAEC
 sed -i "/^        removed {/ {;N s/        removed {\n        }//}" $MODAEC
-sed -i 's/removed { }//g' $MODAEC
+sed -i 's|removed { }||g' $MODAEC
+sed -i 's|removed {}||g' $MODAEC
+sed -i '/removed.*{.*/,/}/d' $MODAEC
+sed -i -n '/^\s*$/!p' $MODAEC
+sed -i '/^[[:space:]]*$/d' $MODAEC
 }
 remove_xml() {
-for RMVS in $RMV; do
-  sed -i "s/\"$RMVS\"/\"removed\"/g" $MODAEX
+for RMV in $RMVS; do
+  sed -i "s|\"$RMV\"|\"removed\"|g" $MODAEX
 done
-sed -i 's/<library name="removed" path="removed"\/>//g' $MODAEX
-sed -i 's/<library name="proxy" path="removed"\/>//g' $MODAEX
-sed -i 's/<effect name="removed" library="removed" uuid="removed"\/>//g' $MODAEX
-sed -i 's/<effect name="removed" uuid="removed" library="removed"\/>//g' $MODAEX
-sed -i 's/<libsw library="removed" uuid="removed"\/>//g' $MODAEX
-sed -i 's/<libhw library="removed" uuid="removed"\/>//g' $MODAEX
-sed -i 's/<apply effect="removed"\/>//g' $MODAEX
-sed -i 's/<library name="removed" path="removed" \/>//g' $MODAEX
-sed -i 's/<library name="proxy" path="removed" \/>//g' $MODAEX
-sed -i 's/<effect name="removed" library="removed" uuid="removed" \/>//g' $MODAEX
-sed -i 's/<effect name="removed" uuid="removed" library="removed" \/>//g' $MODAEX
-sed -i 's/<libsw library="removed" uuid="removed" \/>//g' $MODAEX
-sed -i 's/<libhw library="removed" uuid="removed" \/>//g' $MODAEX
-sed -i 's/<apply effect="removed" \/>//g' $MODAEX
+sed -i 's|<library name="removed" path="removed"/>||g' $MODAEX
+sed -i 's|<library name="proxy" path="removed"/>||g' $MODAEX
+sed -i 's|<effect name="removed" library="removed" uuid="removed"/>||g' $MODAEX
+sed -i 's|<effect name="removed" uuid="removed" library="removed"/>||g' $MODAEX
+sed -i 's|<libsw library="removed" uuid="removed"/>||g' $MODAEX
+sed -i 's|<libhw library="removed" uuid="removed"/>||g' $MODAEX
+sed -i 's|<apply effect="removed"/>||g' $MODAEX
+sed -i 's|<library name="removed" path="removed" />||g' $MODAEX
+sed -i 's|<library name="proxy" path="removed" />||g' $MODAEX
+sed -i 's|<effect name="removed" library="removed" uuid="removed" />||g' $MODAEX
+sed -i 's|<effect name="removed" uuid="removed" library="removed" />||g' $MODAEX
+sed -i 's|<libsw library="removed" uuid="removed" />||g' $MODAEX
+sed -i 's|<libhw library="removed" uuid="removed" />||g' $MODAEX
+sed -i 's|<apply effect="removed" />||g' $MODAEX
+sed -i '/<effectProxy.*removed.*/,/<\/effectProxy>/d' $MODAEX
+sed -i '/removed/d' $MODAEX
+sed -i '/^[[:space:]]*$/d' $MODAEX
+sed -i -n '/^\s*$/!p' $MODAEX
 }
 
-# store
-RMV='libqcompostprocbundle.so
+# remove
+RMVS="libaudioeffectoffload.so
+      libqcompostprocbundle.so
      liboffloadeffect.so
      libbundlewrapper.so
+     hw_acc
+     downmix
+	 libdownmix.so
+	 reverb
+	 bundle
+	 loudness_enhancer
+	 offload_bundle
+	 dirac_gef
+	 libdiraceffect.so
+	 volume_listener
+	 volume 
+	 shoebox
+	 audiosphere
+	 visualizer_sw
+	 visualizer_hw
+	 virtualizer
+	 bassboost
+	 proxy
+ 	equalizer 
+ 	reverb_env_ins 
+ 	reverb_env_aux
+ 	reverb_pre_ins 
+ 	reverb_pre_aux
+ 	libeffectproxy.so
+ 	libvisualizer.so
+ 	libcomvisualizer.so
+ 	d069d9e0-8329-11df-9168-0002a5d5c51b
+ 	7a8044a0-1a71-11e3-a184-0002a5d5c51b
+ 	1d0a1a53-7d5d-48f2-8e71-27fbd10d842c
+ 	7d1580bd-297f-4683-9239-e475b6d1d69f
+	 93f04452-e4fe-41cc-91f9-e475b6d1d69f
+	 3799D6D1-22C5-43C3-B3EC-D664CF8D2F0D
      14804144-a5ee-4d24-aa88-0002a5d5c51b
      8631f300-72e2-11df-b57e-0002a5d5c51b
      2c4a8c24-1581-487f-94f6-0002a5d5c51b
@@ -88,39 +129,55 @@ RMV='libqcompostprocbundle.so
      791fff8b-8129-4655-83a4-59bc61034c3a
      53ef1db5-c0c0-445b-b060-e34d20ebb70a
      b08a0e38-22a5-11e2-b87b-f23c91aec05e
-     libldnhncr.so
-     fa415329-2034-4bea-b5dc-5b381c8d1e2c
-     libvolumelistener.so
-     ring_helper alarm_helper music_helper
-     voice_helper notification_helper
-     08b8b058-0590-11e5-ac71-0025b32654a0
-     0956df94-0590-11e5-bdbe-0025b32654a0
-     09f303e2-0590-11e5-8fdb-0025b32654a0
-     0ace5c08-0590-11e5-ae9e-0025b32654a0
-     0b776dde-0590-11e5-81ba-0025b32654a0
-     libasphere.so
-     184e62ab-2d19-4364-9d1b-c0a40733866c
-     libshoebox.so
-     1eab784c-1a36-4b2a-b7fc-e34c44cab89e
-     libspatialaudio.so
-     cc4677de-ff72-11eb-9a03-0242ac130003'
-     #libdynproc.so
-     #e0e6539b-1781-7261-676f-6d7573696340
-
-# patch audio effects conf
+      volumemonitor_hw
+      052a63b0-f95a-11e9-8f0b-362b9e155667
+      libvolumemonitor_energy.so
+      volumemonitor
+      16311c29-bb53-4415-b7af-ae653e812de8
+      libsamsungSoundbooster_plus_legacy.so libsamsungSoundbooster_plus.so
+      soundbooster_plus
+      50de45f0-5d4c-11e5-a837-0800200c9a66
+      libsamsungSoundbooster_tdm_legacy.so
+      tdm
+      beb1d058-916a-4adf-9cfe-54ee5ba8c4a5
+      libaudiosaplus_sec_legacy.so libaudiosaplus_sec.so
+      soundalive_sec soundalive
+      cf65eb39-ce2f-48a8-a903-ceb818c06745
+      05227ea0-50bb-11e3-ac69-0002a5d5c51b
+      0b2dbc60-50bb-11e3-988b-0002a5d5c51b
+      libmysound_legacy.so libmysound.so
+      mysound dha
+      263a88e0-50b1-11e2-bcfd-0800200c9a66
+      37155c20-50bb-11e3-9fac-0002a5d5c51b
+      3ef69260-50bb-11e3-931e-0002a5d5c51b
+      libmyspace.so
+      myspace sa3d
+      3462a6e0-655a-11e4-8b67-0002a5d5c51b
+      1c91fca0-664a-11e4-b8c2-0002a5d5c51b
+      c7a84e61-eebe-4fcc-bc53-efcb841b4625
+      libswspatializer.so
+      spatializer
+      ccd4cf09-a79d-46c2-9aae-06a1698d6c8f
+      libLifevibes_lvverx.so
+      lmfv fens
+      3b75f00-93ce-11e0-9fb8-0002a5d5c51b
+      d6dbf400-93ce-11e0-bcd7-0002a5d5c51b
+      df0afc20-93ce-11e0-98de-0002a5d5c51b
+      989d9460-413d-11e1-8b0d-0002a5d5c51b
+      cbcc5980-476d-11e1-82ee-0002a5d5c51b
+      libgearvr.so
+      vr360audio vr3d
+      e6388202-e7a4-4c72-b68a-332eeeba269b
+      libswdap_legacy.so
+      libswdht_legacy.so
+      dlb_ht dht
+      b247dfc9-2832-4486-92c0-2db27a3785f5
+      libvoicechanger.so
+      voicechanger
+      25c30101-9542-451f-a064-c3198e187dfc"
 if [ "$MODAEC" ]; then
   remove_conf
 fi
-
-# patch audio effects xml
 if [ "$MODAEX" ]; then
   remove_xml
 fi
-
-
-
-
-
-
-
-
